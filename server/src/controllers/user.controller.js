e2126f4e68835901b770f04b87410a109dba47af
priteshgholap <pngholap2004@gmail.com>
2026-07-09 21:58:50 +0530

    feat: implement user registration

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,6 +1,43 @@
+import User from "../models/User.model.js";
 export const registerUser = async (req,res)=>{
-    res.status(201).json({
-        success:true,
-        message:"User Registration API Working ",
-    });
+    try{
+        const { name, email, password } = req.body;
+
+        if(!name || !email || !password){
+            return res.status(400).json({
+                success:false,
+                message:"All fields are required",
+            });
+        }
+
+        const existingUser=await User.findOne({email});
+        if(existingUser){
+            return res.status(409).json({
+                success:false,
+                message:"User already exists",
+            });
+        }
+
+        const user = await User.create({
+            name,
+            email,
+            password,
+        });
+
+        res.status(201).json({
+            success:true,
+            message:"User registered successfully",
+            user,
+        });
+
+        // res.status(201).json({
+        // success:true,
+        // message:"User Registration API Working ",
+    // });
+    }catch(error){
+        res.status(500).json({
+            success:false,
+            message: error.message,
+        });
+    }
 };
