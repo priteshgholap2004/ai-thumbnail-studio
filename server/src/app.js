8935e98ce245ca55e9fb2e52a6111523fbf3b68e
priteshgholap <pngholap2004@gmail.com>
2026-07-09 18:46:10 +0530

    feat: configure cors middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,20 @@
 import express from "express";
 import logger from "./middleware/logger.middleware.js";
+import cros from "cors";
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(logger);
+
+//cors
+app.use(
+  cors({
+    origin:"https://localhost:5173",
+    credentials:true,
+  })
+);
 
 // Home Route
 app.get("/", (req, res) => {
